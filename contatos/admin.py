e667a1748a4fc481c1e229abd02bc916aea473de
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-05-27 17:43:28 -0300

    update: atualização de admin & views. fix: adicionado os caches no gitignore.

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email',
+                    'categoria', 'data_criacao')
+
+    list_display_links = ('nome', 'sobrenome', 'telefone', 'email',
+                          'categoria')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'categoria', 'email', 'telefone')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
