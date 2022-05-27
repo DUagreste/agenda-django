b0855892ad5509101aa90cdd6bd40d43e646bcc8
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-05-27 18:23:57 -0300

    insert: algumas condicionais.

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,12 +4,14 @@ from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email',
-                    'categoria', 'data_criacao')
+                    'categoria', 'data_criacao', 'mostrar')
 
-    list_display_links = ('nome', 'sobrenome', 'telefone', 'email',
+    list_display_links = ('nome', 'sobrenome', 'email',
                           'categoria')
+
     list_per_page = 10
     search_fields = ('nome', 'sobrenome', 'categoria', 'email', 'telefone')
+    list_editable = ('mostrar', 'telefone')
 
 
 admin.site.register(Categoria)
